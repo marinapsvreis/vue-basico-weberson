122e41feb9ecd0682244032e67f232cd09fc17c3
Marina Portugal <marinapsvreis@gmail.com>
2022-09-20 16:27:55 -0300

    controle de autenticacao para as rotas

--- src/router/index.js
+++ src/router/index.js
@@ -9,4 +9,32 @@ const router = new VueRouter({
   mode: 'history' //remove a tralha do endereço da página
 });
 
+router.beforeEach((to, from, next) => {
+
+  let token = localStorage.getItem('token');
+
+  if (to.name == "Login") {
+    if (token) {
+      next({ name: 'ControleDeProdutosView' })
+    } else {
+      next();
+    }
+  } else if (to.matched.some(rota => rota.meta.requiredAuth)) {
+    if (localStorage.getItem('token') == null) {
+      next({
+        path: "/login",
+        params: { nextURL: to.fullPath }
+      })
+    } else {
+      next();
+    }
+  } else {
+    if (token == null) {
+      next();
+    } else {
+      next({ name: "ControleDeProdutosView" });
+    }
+  }
+})
+
 export default router;
